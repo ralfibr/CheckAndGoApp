54d308ae08a37c380cf9317d25d59cf88f09504b
Raeef Ibrahim <raeef.ibrahim@hva.nl>
2020-05-21 16:29:26 +0200

    Update user status

--- app/src/main/java/com/androidcourse/checkgoapp/SignUp.kt
+++ app/src/main/java/com/androidcourse/checkgoapp/SignUp.kt
@@ -3,13 +3,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
+import com.androidcourse.checkgoapp.model.User
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class SignUp : AppCompatActivity() {
     private var inputEmail: EditText? = null
@@ -18,14 +23,18 @@ class SignUp : AppCompatActivity() {
     private var btnSignUp: Button? = null
     private var btnResetPassword: Button? = null
     private var progressBar: ProgressBar? = null
-
+    private var inputName: EditText? = null
     private var auth : FirebaseAuth?= null
+
+    private lateinit var database: DatabaseReference
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
         this.supportActionBar?.hide();
 
-
+        database = Firebase.database.reference
         //        add my code
         auth = FirebaseAuth.getInstance()
 
@@ -34,20 +43,17 @@ class SignUp : AppCompatActivity() {
         inputEmail = findViewById(R.id.email3) as EditText
         inputPassword = findViewById(R.id.password3) as EditText
         progressBar = findViewById(R.id.progressBar2) as ProgressBar
-        //btnResetPassword = findViewById(R.id.btn_reset_password) as Button
-//
-//        btnResetPassword!!.setOnClickListener(View.OnClickListener {
-//           // startActivity(Intent(this@SingupActivity, ResetPasswordActivity::class.java))
-//        })
+        inputName = findViewById(R.id.name) as EditText
 
         btnSignIn!!.setOnClickListener(View.OnClickListener {
             finish()
         })
         btnSignUp!!.setOnClickListener(View.OnClickListener {
+
             val email = inputEmail!!.text.toString().trim()
             val password = inputPassword!!.text.toString().trim()
-
-            if (TextUtils.isEmpty(email)){
+            val username = inputName!!.text.toString().trim()
+            if (TextUtils.isEmpty(email) ){
                 Toast.makeText(applicationContext,"Enter your email Address!!", Toast.LENGTH_LONG).show()
                 return@OnClickListener
             }
@@ -55,32 +61,46 @@ class SignUp : AppCompatActivity() {
                 Toast.makeText(applicationContext,"Enter your Password", Toast.LENGTH_LONG).show()
                 return@OnClickListener
             }
+
             if (password.length < 6){
                 Toast.makeText(applicationContext,"Password too short, enter mimimum 6 charcters" , Toast.LENGTH_LONG).show()
                 return@OnClickListener
             }
             progressBar!!.setVisibility(View.VISIBLE)
 
-            //create user
-            auth!!.createUserWithEmailAndPassword(email,password)
-                .addOnCompleteListener(this, OnCompleteListener {
-                        task ->
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please enter text in email/pw", Toast.LENGTH_SHORT).show()
+                return@OnClickListener
+            }
+
+            Log.d("MainActivity", "Email is: " + email)
+            Log.d("MainActivity", "Password: $password")
+
+            // Firebase Authentication to create a user with email and password
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (!it.isSuccessful) return@addOnCompleteListener
                     Toast.makeText(this,"User is crated",
-                        Toast.LENGTH_SHORT).show()
-                    progressBar!!.setVisibility(View.VISIBLE)
-
-                    if (!task.isSuccessful){
-                        Toast.makeText(this,"User Not crated", Toast.LENGTH_SHORT).show()
-                        return@OnCompleteListener
-                    }else{
-                        startActivity(Intent(this, MainActivity::class.java))
+                    Toast.LENGTH_SHORT).show()
+                   progressBar!!.setVisibility(View.VISIBLE)
+                    startActivity(Intent(this, MainActivity::class.java))
                         finish()
-                    }
-
-
-                })
-
+                    // else if successful
+                    Log.d("Main", "Successfully created user with uid: ")
+                }
+                .addOnFailureListener{
+                    Log.d("Main", "Failed to create user: ${it.message}")
+                    Toast.makeText(this, "Failed to create user: ${it.message}", Toast.LENGTH_SHORT).show()
+                }
+            writeNewUser(username,email)
         })
+
+    }
+
+    private fun writeNewUser( name: String, email: String?) {
+        val user = User(name, email)
+        database.child("users").child(name).setValue(user)
+
     }
 
 
---
+++ app/src/main/java/com/androidcourse/checkgoapp/model/User.kt
@@ -0,0 +1,6 @@
+package com.androidcourse.checkgoapp.model
+
+data class User(
+    var username: String? = "",
+    var email: String? = ""
+)
